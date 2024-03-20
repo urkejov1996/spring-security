5e800f225ab113c6c897fff7d851f3cf649369b5
urkejov1996 <urkejov1996@gmail.com>
2024-03-20 12:12:26 +0100

    added controller and endpoint for getting csrf token

---
+++ src/main/java/com/springsecurity/controllers/SpringSecurityController.java
@@ -0,0 +1,15 @@
+package com.springsecurity.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringSecurityController {
+
+    @GetMapping("/csrf-token")
+    public CsrfToken retrieveCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
